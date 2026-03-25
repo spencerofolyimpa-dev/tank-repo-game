e29d9d3b326e001118538c69ca093c7dcbb13f2e
Ansoron <143147464+Ansoron@users.noreply.github.com>
2026-03-24 21:43:07 -0700

    Revived Scene & Added Some Camera Movement
    Still some errors regarding the camera, though.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,31 +5,45 @@ public class Player : MonoBehaviour
 {
 
     public float speed = 5f;
-    public float jumpForce = 5f;
+    public float jumpForce = 5f;  
 
     private Rigidbody rb; // Holds reference to player's rigidbody.
+    private PlayerInput playerInput; // Reference to the PlayerInput component.
+    public GameObject playerCamera; // Reference to the player's camera object.
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody>(); 
+        rb = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();  // Executes every frame to continuously update the player's position.
     }
 
-    public void Move(InputAction.CallbackContext context)  // TODO: Figure out how to update it
+
+    /*CONTROLS************************************************************************************************/
+
+    private void Move()
     {
-        Debug.Log(context.ReadValue<Vector2>());
-        Vector2 inputVector = context.ReadValue<Vector2>();
-
-        rb.AddForce(new Vector3(inputVector.x, 0f, inputVector.y) * speed, ForceMode.Force);
+        // Reads the "Move" action value of the "PlayerMovement" input map every frame, then apply it to the player's position.
+        Vector2 inputVector = playerInput.actions["PlayerMovement/Move"].ReadValue<Vector2>();
+        rb.transform.Translate(new Vector3(inputVector.x, 0f, inputVector.y) * speed * Time.deltaTime);
     }
 
-    public void Jump(InputAction.CallbackContext context)
+    public void Look(InputAction.CallbackContext context)  // Connected to the PlayerInput event function call.
+    {
+        Vector2 lookInput = context.ReadValue<Vector2>();
+        Debug.Log("Look input: " + lookInput);
+
+        //!\\ ERROR: The player's body spins but the camera does not folllow. //!\\
+        playerCamera.transform.rotation = Quaternion.Euler(-lookInput.y, transform.rotation.y, transform.rotation.z); // Rotate camera only up and down
+        transform.rotation = Quaternion.Euler(transform.rotation.x, -lookInput.x, transform.rotation.z);
+    }
+    public void Jump(InputAction.CallbackContext context)  // Connected to the PlayerInput event function call.
     {
         // Execute only when the jump button is down
         if (context.performed)
